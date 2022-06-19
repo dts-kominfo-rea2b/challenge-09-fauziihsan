a89a06bd34e533b0936a428fe4fab55f70874c9e
fauziihsan <fauzyihsan@gmail.com>
2022-06-19 09:45:40 +0700

    Menyelesaikan Challenge-09

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,26 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import style from "./contact.module.css";
 
-const Contact = () => {
+
+const Contact = ({data}) => {
+    const { name, email, phone, photo } = data;
     return (
-        <></>
+        <div className={style.contact}>
+            <div className={style.contact__avatar}>
+                <img src={photo} alt={name} />
+            </div>
+            <div className={style.contact__info}>
+                <p className={style.contact__info__txt}>
+                    <b>{name}</b>
+                </p>
+                <div className={style.contact__infodesc}>
+                    <p className={style.contact__infodesc__txt}>{phone}</p>
+                    <p className={style.contact__infodesc__txt}>{email}</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import style from "./header.module.css";
 
 const Header = () => {
     return (
-        <></>
+        <header className={style.header}>
+            <h1 className={style.header__title}>Call a Friend</h1>
+            <p className={style.header__subtitle}>your friendly contact app </p>
+        </header>
     )
 }
 
